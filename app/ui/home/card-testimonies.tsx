6041fd55ac33ab699a25675ecd16a8f008dcba68
Joe-Itax <josephitakala18@gmail.com>
2024-06-09 16:11:31 +0100

    feat/update: create a card-stack component using aceternity-ui library, and update the testimonies section on home page

---
+++ app/ui/home/card-testimonies.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { CardStack } from "@/app/ui/card-stack";
+import { cn } from "@/utils/cn";
+export function CardStackTestimonies() {
+  return (
+    <div className="flex items-center justify-center w-full py-20">
+      <CardStack items={CARDS} />
+    </div>
+  );
+}
+
+// Small utility to highlight the content of specific section of a testimonial content
+export const Highlight = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
+  return (
+    <span
+      className={cn(
+        "font-bold bg-emerald-100 text-emerald-700 dark:bg-emerald-700/[0.2] dark:text-emerald-500 px-1 py-0.5",
+        className
+      )}
+    >
+      {children}
+    </span>
+  );
+};
+
+const CARDS = [
+  {
+    id: 0,
+    name: "Joe Itax",
+    designation: "CEO of Itax Corporation",
+    content: (
+      <p>
+        Genius <Highlight>a joué un rôle déterminant</Highlight> dans la
+        transformation de notre présence en ligne. 🙏
+      </p>
+    ),
+  },
+  {
+    id: 1,
+    name: "John Smith",
+    designation: "CEO of Chic Boutique",
+    content: (
+      <p>
+        <Highlight>Travailler avec Genius</Highlight> a été un{" "}
+        <Highlight>jeu d&apos;enfant</Highlight>.
+      </p>
+    ),
+  },
+  {
+    id: 2,
+    name: "Carmel",
+    designation: "CEO of Cabukaka",
+    content: (
+      <p>
+        <Highlight>Genius</Highlight> est une{" "}
+        <Highlight>agence de développement</Highlight>
+      </p>
+    ),
+  },
+];
