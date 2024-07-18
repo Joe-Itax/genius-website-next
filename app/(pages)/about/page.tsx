ee25a19867a1571416dfcb26e9c7ed25302857ff
Joseph Itakala <josephitakala18@gmail.com>
2024-07-19 00:04:54 +0100

    feat: create and implement the About page

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -1,3 +1,52 @@
+import ConclusionSection from "@/app/ui/contact/conclusion-section";
+import Image from "next/image";
+// import im from "@/public/assets/images/about-page/bg-logo.svg";
 export default function Realisations() {
-  return <div>Realisations</div>;
+  return (
+    <main>
+      <div className="sub-main border-border border-x">
+        <section className="hero bg-[url('/assets/images/our-service-page/bg-heading-our-service.webp')] bg-no-repeat bg-center bg-cover flex flex-col justify-center items-center px-4 py-24 text-center">
+          <h1>À Propos de Nous</h1>
+          <p className="text-xl">
+            Bienvenue chez Genius, où la collaboration, l&apos;expertise et
+            l&apos;orientation client se croisent pour façonner l&apos;avenir de
+            l&apos;innovation numérique.
+          </p>
+        </section>
+        <section className="flex flex-col min-[960px]:flex-row gap-8 min-[960px]:gap-0 justify-center items-center px-4 min-[400px]:px-8 min-[530px]:px-16 py-12">
+          <div className="w-full min-[530px]:w-5/6 min-[960px]:w-1/2 flex flex-col gap-8">
+            <h3 className="text-3xl font-bold">À Propos de Genius</h3>
+            <div>
+              <p>
+                Genius est une agence de produits numériques passionnée par la
+                création d&apos;expériences numériques exceptionnelles. Nous
+                sommes spécialisés dans la conception, l’ingénierie et la
+                gestion de projets, aidant les entreprises à prospérer dans le
+                paysage numérique. Chez Genius, nous suivons un processus
+                structuré et collaboratif pour garantir la livraison réussie de
+                produits numériques exceptionnels. Notre processus combine les
+                meilleures pratiques de l’industrie, une pensée créative et une
+                approche centrée sur le client.
+              </p>
+            </div>
+          </div>
+          <div className="w-full min-[530px]:w-5/6 min-[960px]:w-1/2 flex justify-end">
+            <div className="relative w-full min-[960px]:w-11/12">
+              <Image
+                src={"/assets/images/about-page/bg-logo.webp"}
+                alt=""
+                width={598}
+                height={571}
+                className="w-full"
+              />
+              <div className="bg-primary flex justify-center items-center rounded-full w-16 h-16 pb-2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <span className="text-4xl block font-extrabold">@</span>
+              </div>
+            </div>
+          </div>
+        </section>
+        <ConclusionSection />
+      </div>
+    </main>
+  );
 }
--- app/ui/realisations/realisations.ts
+++ app/ui/realisations/realisations.ts
@@ -16,7 +16,7 @@ export const ourWorks: Work[] = [
     urlOfSite: `https://cabukaka.shop/`,
     descriptionOfProject: `Nous avons développé une plateforme e-commerce visuellement attrayante et facile à utiliser pour Cabukaka, un détaillant renommé de produits électroniques et électroménagers. La plateforme intègre une navigation fluide des produits, un système de gestion des stocks et un système de parrainage, ce qui a entraîné une augmentation des ventes en ligne et la satisfaction des clients.`,
     technologies: `Wordpress, Elementor, WooCommerce`,
-    dateRealisation: `2024`,
+    dateRealisation: `Janvier - Mars 2024`,
   },
 
   {
@@ -26,7 +26,7 @@ export const ourWorks: Work[] = [
     urlOfSite: `https://etudiant2-0.vercel.app/`,
     descriptionOfProject: `Etudiant 2.0 est une plateforme conçue pour rassembler les étudiants congolais, leur permettant de partager et de télécharger des documents pédagogiques variés tels que des livres, des notes de cours, des exercices, des TP et TD, etc. La plateforme prévoit également d'ajouter des fonctionnalités de chat direct, des forums universitaires, un système de mentorat, et bien plus encore.`,
     technologies: `React.js, Express.js, Node.js`,
-    dateRealisation: `2024`,
+    dateRealisation: `Avril - Mai 2024`,
   },
 
   {
@@ -35,8 +35,8 @@ export const ourWorks: Work[] = [
     nameOfSite: `Space Tourism`,
     urlOfSite: `https://space-touriiism.vercel.app/`,
     descriptionOfProject: `Space Tourism était un défi publié sur le site Frontend Mentor. Nous avons développé un site fictif de tourisme spatial, avec une interface utilisateur captivante et des fonctionnalités interactives, permettant aux utilisateurs d'explorer des destinations spatiales imaginaires.`,
-    technologies: `React.js, Node.js, Express.js`,
-    dateRealisation: `2023`,
+    technologies: `HTML5, CSS3, JavaScript`,
+    dateRealisation: `Août 2023`,
   },
 
   {
@@ -46,6 +46,6 @@ export const ourWorks: Work[] = [
     urlOfSite: `https://advice-generatory.vercel.app/`,
     descriptionOfProject: `Advice Generatory est un site interactif qui génère des conseils éclairants pour vous guider à travers la vie. Laissez-vous surprendre par une nouvelle perspective à chaque visite et découvrez des conseils variés pour toutes les situations.`,
     technologies: `React.js`,
-    dateRealisation: `2023`,
+    dateRealisation: `Janvier 2024`,
   },
 ];
